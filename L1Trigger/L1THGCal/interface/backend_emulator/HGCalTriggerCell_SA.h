f607455e6eec5a14a74d4a17d7bbb418d1c70abb
Louis Portales <louis.portales@gmail.com>
2021-11-26 16:28:47 +0100

    Stage 1 emulator - initial commit

---
+++ L1Trigger/L1THGCal/interface/backend_emulator/HGCalTriggerCell_SA.h
@@ -0,0 +1,83 @@
+#ifndef L1Trigger_L1THGCal_HGCalTriggerCell_SA_h
+#define L1Trigger_L1THGCal_HGCalTriggerCell_SA_h
+
+#include <vector>
+#include <memory>
+#include <iostream>
+namespace l1thgcfirmware {
+
+  class HGCalTriggerCell {
+  public:
+    HGCalTriggerCell() : HGCalTriggerCell(false, false, 0, 0, 0, 0) {}
+    HGCalTriggerCell(bool frameValid,
+                 bool dataValid,
+                 unsigned int rOverZ,
+                 unsigned int phi,
+                 unsigned int layer,
+                 unsigned int energy)
+        : clock_(0),
+          index_(0),
+          rOverZ_(rOverZ),
+          layer_(layer),
+          energy_(energy),
+          phi_(phi),
+          sortKey_(0),
+          deltaR2_(0),
+          dX_(0),
+          Y_(0),
+          frameValid_(frameValid),
+          dataValid_(dataValid) {}
+
+    ~HGCalTriggerCell(){}
+
+    // Setters
+    void setClock( const unsigned int clock ) { clock_ = clock; }
+    void addLatency( const unsigned int latency ) { clock_ += latency; }
+    void setIndex( const unsigned int index ) { index_ = index; }
+    void setSortKey( const unsigned int sortKey ) { sortKey_ = sortKey; }
+    void setDX( const int dX ) { dX_ = dX; }
+    void setY( const unsigned int Y ) { Y_ = Y; }
+    void setDeltaR2( const unsigned int deltaR2 ) { deltaR2_ = deltaR2; }
+
+    // Getters
+    unsigned int clock() const { return clock_; }
+    unsigned int index() const { return index_; }
+    unsigned int rOverZ() const { return rOverZ_; }
+    unsigned int layer() const { return layer_; }
+    unsigned int energy() const { return energy_; }
+    unsigned int phi() const { return phi_; }
+    unsigned int sortKey() const { return sortKey_; }
+    unsigned int deltaR2() const { return deltaR2_; }
+    int dX() const { return dX_; }
+    unsigned int Y() const { return Y_; }
+    bool frameValid() const { return frameValid_; }
+    bool dataValid() const { return dataValid_; }
+
+    // Operators
+    bool operator==(const HGCalTriggerCell& rhs) const;
+    bool operator==(const std::shared_ptr<HGCalTriggerCell>& rhs) const;
+
+  private:
+    unsigned int clock_;
+    unsigned int index_;
+    unsigned int rOverZ_;
+    unsigned int layer_;
+    unsigned int energy_;
+    unsigned int phi_;
+    unsigned int sortKey_;
+    unsigned int deltaR2_;
+    int dX_;
+    unsigned int Y_;
+    bool frameValid_;
+    bool dataValid_;
+
+  };
+
+  typedef std::vector<HGCalTriggerCell> HGCalTriggerCellSACollection;
+  typedef std::shared_ptr<HGCalTriggerCell> HGCalTriggerCellSAPtr;
+  typedef std::vector<HGCalTriggerCellSAPtr> HGCalTriggerCellSAPtrCollection;
+  typedef std::vector< std::vector<std::shared_ptr<HGCalTriggerCell> > > HGCalTriggerCellSAPtrCollections;
+  typedef std::vector< std::vector< std::vector<std::shared_ptr<HGCalTriggerCell> > > > HGCalTriggerCellSAPtrCollectionss;
+}  // namespace l1thgcfirmware
+
+#endif
