97c6a843f05c939b5e9326042d4249c0ed1deca9
Louis Portales <louis.portales@gmail.com>
2021-12-17 15:22:11 +0100

    reinclude folder removed by mistake

---
+++ L1Trigger/L1THGCal/src/backend_emulator/HGCalStage1SortingAlg_SA.cc
@@ -0,0 +1,380 @@
+/*************************************************************************************************/
+// C++ adaptation (w/ minimal changes) of C code from:
+// ******************************
+// Author L. Pacheco Rodriguez, laura.pacheco-rodriguez@llr.in2p3.fr
+// Collaborators: T. Romanteau, J.B. Sauvan, F. Thiant
+// Sept-2018
+// Laboratoire Leprince-Ringuet - CNRS
+// Universite Paris-Saclay
+//************************************************************************************************/
+
+#include "L1Trigger/L1THGCal/interface/backend_emulator/HGCalStage1SortingAlg_SA.h"
+
+#include <iostream>
+
+using namespace l1thgcfirmware;
+
+//****************************************TOP LEVEL FUNCTION****************************************
+//-------------------------------------------------------------------------------------------------/
+// It performs Sorting and selection: sorts N input energies, and selects the M highest
+//
+// @param arr_input - list of elements to sort (input)
+// @param arr_output - list of elements sorted (output)
+// @param arr_adresses - list of adresses of elements selected (output). Address goes from 0 to N
+//
+//-------------------------------------------------------------------------------------------------/
+void HGCalStage1SortingAlg_SA::sorting(datain_t& arr_input, dataout_t& arr_output, adressout_t& arr_adresses) const {
+
+  //Variables declaration
+  datasorter_t arr_tmp(NS);
+  datamergera_t arr_tmp_so(NMA), arr_tmp_so1(NMA), arr_tmp_so2(NMA);
+  datamergerb_t arr_tmp_mo(NMB);
+  adresssorter_t arr_tmp_adresses(NS);
+  adressmergera_t arr_tmp_so_adresses(NMA), arr_tmp_so1_adresses(NMA), arr_tmp_so2_adresses(NMA);
+  adressmergerb_t arr_tmp_mo_adresses(NMB);
+
+  unsigned cs; //counter for sorter blocks
+  unsigned cm; //counter for merger blocks
+  unsigned i;	//Index for loops
+
+  //Generate four "sorter blocks" of NS inputs each-----------------------------------------------
+  for (cs=0; cs<4; cs++) {
+    //Replica of entry array. Important for a correct HW synthesis
+    //It builds a list of NS elements
+    for (i = 0; i < NS; i++) {
+      arr_tmp[i]=arr_input[i+cs*NS];
+      arr_tmp_adresses[i]=i+cs*NS;
+    }
+
+    //Call to function that sorts NS inputs
+    sorter(arr_tmp, arr_tmp_adresses);
+
+    //Arrange 4 sorted lists in 2 arrays of NMA elements each
+    if (cs==0 || cs==1) {
+      for (i = 0; i < NMA/2; i++) {
+	arr_tmp_so1[i*2+cs]=arr_tmp[i];
+	arr_tmp_so1_adresses[i*2+cs]=arr_tmp_adresses[i];
+      }
+    } else if (cs==2 || cs==3) {
+      for (i = 0; i < NMA/2; i++) {
+	arr_tmp_so2[(i-1)*2+cs]=arr_tmp[i];
+	arr_tmp_so2_adresses[(i-1)*2+cs]=arr_tmp_adresses[i];
+      }
+    }
+
+  }//End of block sorter---------------------------------------------------------------------------
+
+  //Generate two "merger blocks" of NMA inputs each-------------------------------------------------
+  for (cm=0; cm<2; cm++) {
+
+    //It builds a list of NMA inputs
+    if (cm==0) {
+      for (i = 0; i < NMA; i++) {
+	arr_tmp_so[i]=arr_tmp_so1[i];
+	arr_tmp_so_adresses[i]=arr_tmp_so1_adresses[i];
+      }
+    } else if(cm==1) {
+      for (i = 0; i < NMA; i++) {
+	arr_tmp_so[i]=arr_tmp_so2[i];
+	arr_tmp_so_adresses[i]=arr_tmp_so2_adresses[i];
+      }
+    }
+
+    //Call to function that merges NMA inputs
+    mergerA(arr_tmp_so, arr_tmp_so_adresses);
+
+    //Arrange 2 sorted lists in 1 array of NMB elements
+    if (cm==0) {
+      for (i = 0; i < NMB/2; i++) {
+	arr_tmp_mo[i*2]=arr_tmp_so[i];
+	arr_tmp_mo_adresses[i*2]=arr_tmp_so_adresses[i];
+      }
+    } else if(cm==1) {
+      for (i = 0; i < NMB/2; i++) {
+	arr_tmp_mo[i*2+1]=arr_tmp_so[i];
+	arr_tmp_mo_adresses[i*2+1]=arr_tmp_so_adresses[i];
+      }
+    }
+
+  }//End of block for merging-------------------------------------------------------------------
+
+
+  //Generates 1 "merger block" of NMB inputs ----------------------------------------------------
+  //Call to function to merge
+  mergerB(arr_tmp_mo, arr_tmp_mo_adresses);
+
+
+  //Replica of output array. Important for a correct HW synthesis
+  for (i = 0; i < M; i++) {
+    arr_output[i]=arr_tmp_mo[i];
+    arr_adresses[i]=arr_tmp_mo_adresses[i];
+  }
+
+}
+
+//*********************************************FUNCTIONS*******************************************
+//-------------------------------------------------------------------------------------------------/
+// Sorter algorithm of NS inputs
+// Based on odd-even Batcher algorithm
+// For more details, see:
+// The Art Of Computer Programming, D. Knuth - Sorting and Searching (2nd edition Volume 3), 1998, page 111
+//-------------------------------------------------------------------------------------------------/
+
+void HGCalStage1SortingAlg_SA::sorter(data_to_sort_t& list_sorter,  adress_t& list_adresses) const {
+
+  unsigned tmp_data, tmp_adress;
+  unsigned i, p, q, r, d, constant_bound;
+  datasorter_t list_sorter_tmp(NS);
+  adresssorter_t list_adresses_tmp(NS);
+
+  //Replica of entry array. Important for a correct HW synthesis
+  for (i = 0; i < NS; i++) {
+    list_sorter_tmp[i]=list_sorter[i];
+    list_adresses_tmp[i]=list_adresses[i];
+  }
+
+  //Calculate bound for loop, which is 2^[log2(NS)]
+  constant_bound=loop_bound_for_sorter();
+
+  //Performed for p=2^(n-1), 2^(n-2), ... , 1
+  for (p=constant_bound,q=constant_bound,r=0,d=p  ;  p>=1  ;  p=p/2,q=constant_bound,r=0,d=p ) {
+
+    //Performed for q=2^(n-1), 2^(n-2), ... , p
+    for (;  q>=p  ;  d=q-p,r=p,q=q/2) {
+      //counter_levels++;
+
+      //Parcour the list_sorter of elements to do exchanges
+      for (i=0  ;  i<(NS-d)  ;  i++ ) {
+
+	if ((i & p)==r) {
+	  //counter_comparators++;
+
+	  //Compare & Exchange
+	  if (list_sorter_tmp[i]<list_sorter_tmp[i+d]) {
+
+	    tmp_data=list_sorter_tmp[i];
+	    list_sorter_tmp[i]=list_sorter_tmp[i+d];
+	    list_sorter_tmp[i+d]=tmp_data;
+
+	    tmp_adress=list_adresses_tmp[i];
+	    list_adresses_tmp[i]=list_adresses_tmp[i+d];
+	    list_adresses_tmp[i+d]=tmp_adress;
+	  }
+
+	}
+
+      }//Label2
+    }//Label1
+  }//Label0
+
+  //Replica of output array. Important for correct HW synthesis
+  for (i = 0; i < NS; i++) {
+    list_sorter[i]=list_sorter_tmp[i];
+    list_adresses[i]=list_adresses_tmp[i];
+  }
+
+}
+
+//-------------------------------------------------------------------------------------------------/
+// Merger algorithm of NMA inputs
+// Based on odd-even Batcher algorithm
+// For more details, see:
+// The Art Of Computer Programming, D. Knuth - Sorting and Searching (2nd edition Volume 3), 1998, page 111
+//
+//-------------------------------------------------------------------------------------------------/
+void HGCalStage1SortingAlg_SA::mergerA(data_to_merge_t& list_merger, adress_t& list_adresses) const {
+
+  unsigned tmp, tmp_adress;
+  unsigned i, p, q, r, d;
+  datamergera_t list_merger_tmp(NMA);
+  adressmergera_t list_adresses_tmp(NMA);
+
+  //Replica of entry array. Important for correct HW synthesis
+  for (i = 0; i < NMA; i++) {
+    list_merger_tmp[i]=list_merger[i];
+    list_adresses_tmp[i]=list_adresses[i];
+  }
+
+  //Variables initialization for nested loops
+  p=1;
+  q=loop_bound_for_mergera();
+  r=0;
+  d=p;
+
+  //Performed for q=2^(n-1), 2^(n-2), ... , p
+  for (;  q>=p  ;  d=q-p,r=p,q=q/2) {
+    //counter_levels++;
+
+    //Parcour the list_sorter of elements to do exchanges
+    for (i=0  ;  i<(NMA-d)  ;  i++ ) {
+
+      if ((i & p)==r) {
+	//counter_comparators++;
+
+	//Compare & Exchange
+	if (list_merger_tmp[i]<list_merger_tmp[i+d]) {
+	  tmp=list_merger_tmp[i];
+	  list_merger_tmp[i]=list_merger_tmp[i+d];
+	  list_merger_tmp[i+d]=tmp;
+
+	  tmp_adress=list_adresses_tmp[i];
+	  list_adresses_tmp[i]=list_adresses_tmp[i+d];
+	  list_adresses_tmp[i+d]=tmp_adress;
+	}
+
+      }
+
+    }//Label2
+  }//Label1
+
+
+  //Replica of output array. Important for correct HW synthesis
+  for (i = 0; i < NMA; i++) {
+    list_merger[i]=list_merger_tmp[i];
+    list_adresses[i]=list_adresses_tmp[i];
+  }
+
+}
+
+//-------------------------------------------------------------------------------------------------/
+// Merger algorithm of NMB inputs
+// Based on odd-even Batcher algorithm
+// For more details, see:
+// The Art Of Computer Programming, D. Knuth - Sorting and Searching (2nd edition Volume 3), 1998, page 111
+//
+//-------------------------------------------------------------------------------------------------/
+void HGCalStage1SortingAlg_SA::mergerB(data_to_merge_t& list_merger, adress_t& list_adresses) const {
+
+  unsigned tmp, tmp_adress;
+  unsigned i, p, q, r, d;
+  datamergerb_t list_merger_tmp(NMB);
+  adressmergerb_t list_adresses_tmp(NMB);
+
+  //Replica of entry array. Important for correct HW synthesis
+  for (i = 0; i < NMB; i++) {
+    list_merger_tmp[i]=list_merger[i];
+    list_adresses_tmp[i]=list_adresses[i];
+  }
+
+  //Variables initialization for nested loops
+  p=1;
+  q=loop_bound_for_mergerb();
+  r=0;
+  d=p;
+
+  //Performed for q=2^(n-1), 2^(n-2), ... , p
+  for (;  q>=p  ;  d=q-p,r=p,q=q/2) {
+    //counter_levels++;
+
+    //Parcour the list_sorter of elements to do exchanges
+    for (i=0  ;  i<(NMB-d)  ;  i++ ) {
+
+      if ((i & p)==r) {
+	//counter_comparators++;
+
+	//Compare & Exchange
+	if (list_merger_tmp[i]<list_merger_tmp[i+d]) {
+	  tmp=list_merger_tmp[i];
+	  list_merger_tmp[i]=list_merger_tmp[i+d];
+	  list_merger_tmp[i+d]=tmp;
+
+	  tmp_adress=list_adresses_tmp[i];
+	  list_adresses_tmp[i]=list_adresses_tmp[i+d];
+	  list_adresses_tmp[i+d]=tmp_adress;
+	}
+
+      }
+
+    }//Label2
+  }//Label1
+
+
+  //Replica of output array. Important for correct HW synthesis
+  for (i = 0; i < NMB; i++) {
+    list_merger[i]=list_merger_tmp[i];
+    list_adresses[i]=list_adresses_tmp[i];
+  }
+
+}
+
+//-------------------------------------------------------------------------------------------------/
+//  Calculate 2^[ceil(log2(NS))-1], which is a loop bound for the sorter algorithm
+//
+//-------------------------------------------------------------------------------------------------/
+unsigned HGCalStage1SortingAlg_SA::loop_bound_for_sorter() const {
+
+  unsigned res=0;
+  unsigned ns_modified=NS;
+
+  if (NS==1) return (unsigned)1; //for some reason this is needed in CMSSW
+
+  //calculate log2(NS): shift to right until NS == 1 for searching position of MSB
+  while (ns_modified>>=1) { ++res; }
+
+  //If 2^[log2(NS)] < NS, round to next integer
+  if ((unsigned)1<<res < NS) { res++; }
+
+  //return 2^(res-1)
+  return (unsigned)1<<(res-1);
+}
+
+
+//-------------------------------------------------------------------------------------------------/
+//  Calculate 2^[ceil(log2(NM))-1], which is a loop bound for the merger algorithm
+//
+//-------------------------------------------------------------------------------------------------/
+unsigned HGCalStage1SortingAlg_SA::loop_bound_for_mergera() const {
+
+  unsigned res=0;
+  unsigned nm_modified=NMA;
+
+  if (NMA==1) return (unsigned)1;//for some reason this is needed in CMSSW
+
+  //calculate log2(NM): shift to right until NS == 1 for searching position of MSB
+  while (nm_modified>>=1) { ++res; }
+
+  //If 2^[log2(NM)] < NM, round to next integer
+  if ((unsigned)1<<res < NMA) { res++; }
+
+  //return 2^(res-1)
+  return (unsigned)1<<(res-1);
+}
+
+//-------------------------------------------------------------------------------------------------/
+//  Calculate 2^[ceil(log2(NM))-1], which is a loop bound for the merger algorithm
+//
+//-------------------------------------------------------------------------------------------------/
+unsigned HGCalStage1SortingAlg_SA::loop_bound_for_mergerb() const {
+
+  unsigned res=0;
+  unsigned nm_modified=NMB;
+
+  if (NMB==1) return (unsigned)1;//for some reason this is needed in CMSSW
+
+  //calculate log2(NM): shift to right until NS == 1 for searching position of MSB
+  while (nm_modified>>=1) { ++res; }
+
+  //If 2^[log2(NM)] < NM, round to next integer
+  if ((unsigned)1<<res < NMB) { res++; }
+
+  //return 2^(res-1)
+  return (unsigned)1<<(res-1);
+}
+//-------------------------------------------------------------------------------------------------/
+//  Calculate log base 2 of i and round the result to next integer
+//  Equivalent to res=ceil(log(i)/log(2))
+//
+//-------------------------------------------------------------------------------------------------/
+unsigned HGCalStage1SortingAlg_SA::log2_rounded (unsigned i) const {
+
+  unsigned res=0;
+  unsigned tmp=i;
+
+  //Shift to right untill i == 1 for searching position of MSB
+  while (i>>=1) { ++res; }
+
+  //If 2^res < i, round
+  if ((unsigned)1<<res < tmp) { res++; }
+
+  return res;
+}
---
+++ L1Trigger/L1THGCal/src/backend_emulator/HGCalStage1TruncationImpl_SA.cc
@@ -0,0 +1,118 @@
+#include "L1Trigger/L1THGCal/interface/backend_emulator/HGCalStage1TruncationImpl_SA.h"
+#include <cmath>
+#include <iostream>
+
+using namespace l1thgcfirmware;
+
+HGCalStage1TruncationImplSA::HGCalStage1TruncationImplSA() {}
+
+unsigned HGCalStage1TruncationImplSA::run(const l1thgcfirmware::HGCalTriggerCellSACollection& tcs_in,
+                                          const l1thgcfirmware::Stage1TruncationConfig& theConf,
+                                          l1thgcfirmware::HGCalTriggerCellSACollection& tcs_out) const {
+
+  //unsigned sector120 = theConf.phiSector();
+  std::unordered_map<unsigned, std::vector<l1thgcfirmware::HGCalTriggerCell>> tcs_per_bin;
+
+  // configuation:
+  bool do_truncate = theConf.doTruncate();
+  double rozmin = theConf.rozMin() ; // "magic numbers" needed for s1 input format
+  double rozmax = theConf.rozMax() ; // "magic numbers" needed for s1 input format
+  unsigned rozbins = theConf.rozBins();
+  const std::vector<unsigned>& maxtcsperbin = theConf.maxTcsPerBin();
+  const std::vector<double>& phiedges = theConf.phiEdges();
+
+  constexpr double margin = 1.001;
+  double roz_bin_size = (rozbins > 0 ? (rozmax - rozmin) * margin / double(rozbins) : 0.);
+
+  // group TCs per (r/z, phi) bins
+  for (const auto& tc : tcs_in) {
+    double roverz = tc.rOverZ() / 10000.; // undo magic number
+    roverz = (roverz < rozmin ? rozmin : roverz);
+    roverz = (roverz > rozmax ? rozmax : roverz);
+
+    unsigned roverzbin = (roz_bin_size > 0. ? unsigned((roverz - rozmin) / roz_bin_size) : 0);
+    double phi = tc.phi()/10000.;
+    int phibin = phiBin(roverzbin, phi, phiedges);
+    if (phibin < 0)
+      return 1;
+    uint32_t packed_bin = packBin(roverzbin, phibin);
+
+      tcs_per_bin[packed_bin].push_back(tc);
+  }
+
+  for (auto& bin_tcs : tcs_per_bin) {
+
+    unsigned roverzbin = 0;
+    unsigned phibin = 0;
+    unpackBin(bin_tcs.first, roverzbin, phibin);
+
+    const unsigned ntcin = smallerMultOfFourGreaterThan(bin_tcs.second.size());
+    const unsigned ntcout = (do_truncate ? maxtcsperbin[roverzbin] : bin_tcs.second.size());
+
+    l1thgcfirmware::HGCalStage1SortingAlg_SA tcSorter(ntcin,ntcout);
+
+    std::vector<unsigned> theTCsIn_mipt(ntcin);
+    std::vector<unsigned> theTCsOut_mipt(ntcout);
+    std::vector<unsigned> theTCsOut_addr(ntcout);
+
+    for (unsigned i=0; i<ntcin; ++i) {
+      if(i<bin_tcs.second.size())
+        theTCsIn_mipt[i] = bin_tcs.second.at(i).energy();
+      else
+        theTCsIn_mipt[i] = 0;
+    }
+
+    tcSorter.sorting(theTCsIn_mipt, theTCsOut_mipt, theTCsOut_addr);
+
+    for (const unsigned& tcid : theTCsOut_addr) {
+
+      if (tcid<bin_tcs.second.size()) {
+        tcs_out.push_back(bin_tcs.second.at(tcid));
+      }
+    }
+  }
+
+  return 0;
+}
+
+unsigned HGCalStage1TruncationImplSA::packBin(unsigned roverzbin, unsigned phibin) const {
+  unsigned packed_bin = 0;
+  packed_bin |= ((roverzbin & mask_roz_) << offset_roz_);
+  packed_bin |= (phibin & mask_phi_);
+  return packed_bin;
+}
+
+void HGCalStage1TruncationImplSA::unpackBin(unsigned packedbin, unsigned& roverzbin, unsigned& phibin) const {
+  roverzbin = ((packedbin >> offset_roz_) & mask_roz_);
+  phibin = (packedbin & mask_phi_);
+}
+
+int HGCalStage1TruncationImplSA::phiBin(unsigned roverzbin, double phi, const std::vector<double>& phiedges) const {
+  unsigned phi_bin = 0;
+  if (roverzbin >= phiedges.size())
+    return -1;
+  double phi_edge = phiedges[roverzbin];
+  if (phi > phi_edge)
+    phi_bin = 1;
+  return phi_bin;
+}
+
+double HGCalStage1TruncationImplSA::rotatedphi(double phi, unsigned sector) const {
+  if (sector == 1) {
+    if (phi < M_PI and phi > 0)
+      phi = phi - (2. * M_PI / 3.);
+    else
+      phi = phi + (4. * M_PI / 3.);
+  } else if (sector == 2) {
+    phi = phi + (2. * M_PI / 3.);
+  }
+  return phi;
+}
+
+unsigned HGCalStage1TruncationImplSA::smallerMultOfFourGreaterThan(unsigned N) const {
+  unsigned remnant = (N+4)%4;
+  if (remnant==0)
+    return N;
+  else
+    return (N+4-remnant);
+}
